df79f0ce5fa34c181b761200d1def1fb56164bc8
panel14 <xdotawinx@gmail.com>
2021-04-21 20:19:37 +0300

    Uncomment Gradient Function

--- main.py
+++ main.py
@@ -60,31 +60,31 @@ def CheckFunction(str_form):
         return True
 
 
-# def GetDx(coefs, point):
-#     return 2 * point.x / (coefs[0] * coefs[2])
-#
-#
-# def GetDy(coefs, point):
-#     return 2 * point.y / (coefs[1] * coefs[2])
-#
-#
-# def GetFunctionValue(coefs, point):
-#     return (point.x ** 2 / coefs[0] ** 2 + point.y ** 2 / coefs[1] ** 2)/coefs[2]
-#
-#
-# def Go(point, coefs):
-#     previos_p = point
-#     current_p = Point(0, 0)
-#     x_bord = 99999
-#     y_bord = 99999
-#     while (x_bord >= EPS) and (y_bord >= EPS):
-#         current_p.x = previos_p.x - RATE * GetDx(coefs, previos_p)
-#         current_p.y = previos_p.y - RATE * GetDy(coefs, previos_p)
-#         x_bord = abs(current_p.x - previos_p.x)
-#         y_bord = abs(current_p.y - previos_p.y)
-#         previos_p = current_p
-#     z = GetFunctionValue(coefs, current_p)
-#     print('Minimum: (', format(current_p.x, '.6f'), ';', format(current_p.y, '.6f'), ';', format(z, '.6f'), ')')
+def GetDx(coefs, point):
+    return 2 * point.x / (coefs[0] * coefs[2])
+
+
+def GetDy(coefs, point):
+    return 2 * point.y / (coefs[1] * coefs[2])
+
+
+def GetFunctionValue(coefs, point):
+    return (point.x ** 2 / coefs[0] ** 2 + point.y ** 2 / coefs[1] ** 2)/coefs[2]
+
+
+def Go(point, coefs):
+    previos_p = point
+    current_p = Point(0, 0)
+    x_bord = 99999
+    y_bord = 99999
+    while (x_bord >= EPS) and (y_bord >= EPS):
+        current_p.x = previos_p.x - RATE * GetDx(coefs, previos_p)
+        current_p.y = previos_p.y - RATE * GetDy(coefs, previos_p)
+        x_bord = abs(current_p.x - previos_p.x)
+        y_bord = abs(current_p.y - previos_p.y)
+        previos_p = current_p
+    z = GetFunctionValue(coefs, current_p)
+    print('Minimum: (', format(current_p.x, '.6f'), ';', format(current_p.y, '.6f'), ';', format(z, '.6f'), ')')
 
 
 func_str = input('Enter function\n')
