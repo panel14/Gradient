b87b847344158ea0fbefd664beb2c251a36c8ad2
Samsonov Stas <72801720+Slabhide@users.noreply.github.com>
2021-05-16 17:42:26 +0300

    Добавлена консольная часть

--- main.py
+++ main.py
@@ -87,11 +87,94 @@ def Go(point, coefs):
     print('Minimum: (', format(current_p.x, '.6f'), ';', format(current_p.y, '.6f'), ';', format(z, '.6f'), ')')
 
 
-func_str = input('Enter function\n')
-lp_str = input('Input coordinates of point. Format: x y z\n')
-lp_str = lp_str.strip()
-lp_arr = lp_str.split()
-previos_p = Point(float(lp_arr[0]), float(lp_arr[1]))
-coefs = GetFunctionCoefs(func_str)
-print(coefs)
-# Go(previos_p, coefs)
+code = input('Отправьте команду. Чтобы увидеть полный список команд и параметров введите "show":\n')
+repeat_code = ''
+f = ''
+p = ''
+while code != 'end':
+    if code == 'find_extra':
+        func_str = input('Введите функцию. Формат: x^2/2 - y^2/2 = 2z. '
+                         'Чтобы изменить формат ввода измените параметр "FORMAT_COEFS" на True.\n')
+
+        if func_str == 'f':
+            func_str = f
+            print('Введено: ', f)
+
+        get_function_coefs(func_str, coefs)
+        axes = show_function()
+        lp_str = input('Введите координаты точки. Формат: x y. Координата z найдётся автоматически.\n')
+
+        if lp_str == 't':
+            lp_str = t
+            print('Введено: ', t)
+
+        lp_str = lp_str.strip()
+        lp_arr = lp_str.split()
+        previous_p = Point(float(lp_arr[0]), float(lp_arr[1]))
+        get_minimum(previous_p, axes)
+        plt.show()
+        f = func_str
+        t = lp_str
+        coefs = [1, 1, 1]
+    elif code == 'graph':
+        func_str = input('Введите функцию:\n')
+
+        if func_str == 'f':
+            func_str = f
+            print('Введено: ', f)
+
+        get_function_coefs(func_str, coefs)
+        show_function()
+        plt.show()
+        f = func_str
+    elif code == 'show':
+        print('Команды:\n'
+              '"find_extra" - найти минимум функции.\n'
+              '"graph" - отобразить график функции.\n'
+              '"end" - закрыть приложение.\n'
+              '"r" - повтор прошлой команды.\n'
+              '"f" - ввод ранее введённой функции\n'
+              '"t" - ввод ранее введённой точки\n'
+              'Параметры:\n'
+              '"IS_MINIMUM" - определяет, ищется максимум или минимум функции. Формат ввода: IS_MINIMUM = True.'
+              'По умолчанимю True.\n'
+              '"FORMAT_COEFS" - определяет формат ввода - функция вводится целиком или только коэфициенты. '
+              'Формат ввода: FORMAT_COEFS = True. По умолчанию True.\n'
+              '"HYPER" - задаёт коэффициент обучения. По умолчанию равен 0.01.\n'
+              '"EPS" - задаёт параметр останова алгоритма. По умолчанию равен 0.0000001.\n'
+              '"CRITERION" - задаёт критерий останова. По умолчанию равен 1.\n')
+    elif code == "r":
+        code = repeat_code
+        is_repeat = True
+    elif code.__contains__('IS_MINIMUM'):
+        if code.__contains__('True'):
+            is_minimum = True
+            print('Поиск минимума.')
+        else:
+            is_minimum = False
+            print('Поиск максимума.')
+    elif code.__contains__('HYPER'):
+        rate = float(code.split('=')[1])
+        print('Значение коэффициента обучения изменено.')
+        is_param = True
+    elif code.__contains__('EPS'):
+        eps = float(code.split('=')[1])
+        print('Значение критерия останова изменено.')
+        is_param = True
+    elif code.__contains__('CRITERION'):
+        criterion = int(code.split('=')[1])
+        print('Критерий останова изменен. Текущий критерий: ')
+        if criterion == 1:
+            print('|f(x1,y1) - f(x0,y0)| < EPS')
+        else:
+            print('|x1 - x0| < EPS AND |y1 - y0| < EPS')
+        is_param = True
+    else:
+        print('Неизвестная команда')
+        is_param = True
+    if not is_param:
+        repeat_code = code
+    if not is_repeat:
+        code = input('Отправьте команду:\n')
+    is_repeat = False
+    is_param = False
